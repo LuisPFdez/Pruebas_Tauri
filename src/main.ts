8fc984254c80ac235d861a747e3870696594ef7e
Luis Puente Fernández <luis.p.fdez9@gmail.com>
2024-03-24 23:05:03 +0100

    Modificado main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ConfirmDialogOptions, confirm } from "@tauri-apps/api/dialog";
 
 import { trace, info, error, attachConsole } from "tauri-plugin-log-api";
 
+//Funcion que de muestra para capturar los logs atraves de los eventos
 async function log() {
 
   interface evento {
@@ -13,22 +14,23 @@ async function log() {
 
   //La funcion attachConsole, es en en general un simple escucha de eventos que luego decora
   //haciendo diferentes parseos del payload y usando distitos logs en funcion del nivel
-  let evento = await listen("log://log", ({payload}) => {
-      let {level, message} = payload as evento;
-      console.log("Nivel: ", level, ", Mensaje: ", message);
+  let evento = await listen("log://log", ({ payload }) => {
+    let { level, message } = payload as evento;
+    console.log("Nuevo eventfo log nivel: ", level, ", Mensaje: ", message);
   });
 
   trace("Trace");
   info("Info");
   error("Error");
-  
+
+  //Para dejar de escuchar los eventos bastara con ejecutar la funcion que retorna listen()
   evento();
 }
 
 let greetInputEl: HTMLInputElement | null;
 let greetMsgEl: HTMLElement | null;
 
-listen("cerrar_ventanas", async (evento) => {
+listen("cerrar_ventanas", async (_) => {
   let opcionesDialogo: ConfirmDialogOptions = {
     title: "Salir de la aplicacion",
     okLabel: "Aceptar",
@@ -60,6 +62,14 @@ async function pruebas() {
 
 }
 
+window.onload = async function () {
+  //Llama a la funcion log
+  await log();
+
+  //Usa el sistema de escucha de logs de tauri
+  attachConsole();
+}
+
 window.addEventListener("DOMContentLoaded", () => {
   greetInputEl = document.querySelector("#greet-input");
   greetMsgEl = document.querySelector("#greet-msg");
@@ -69,6 +79,6 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
   document.querySelector("#pruebas")?.addEventListener("click", () => {
-    log();
+    pruebas();
   })
 });
