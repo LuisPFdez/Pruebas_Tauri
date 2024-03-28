f292bff999dabdcd2d24cdf61a3175be3cf199d5
Luis Puente Fernández <luis.p.fdez9@gmail.com>
2024-03-28 23:01:41 +0100

    abrir_nueva_ventana modifica la ruta cuando este abierta

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-pub(crate) mod errores;
+mod errores;
 use errores::{CustomError, TauriError};
 
 use log::info;
-use std::path::{Component, PathBuf};
+use std::path::PathBuf;
 use tauri::{CustomMenuItem, Manager, Menu, Submenu, WindowMenuEvent};
 use tauri_plugin_log::LogTarget;
 
@@ -25,39 +25,42 @@ fn crear_barra_de_menu() -> Menu {
 }
 
 #[tauri::command]
-async fn abrir_nueva_ventana(handle: tauri::AppHandle, ruta: &str, titulo: &str) -> Result<&'static str, CustomError> {
-    let mut ruta_buf: PathBuf;
-    
-    let r = ruta.parse::<PathBuf>()?;
-
-    if r.components().any(|x| x.eq(&Component::ParentDir)) {
-        return Err(
-            tauri::Error::InvalidWindowUrl("No se puede acceder a direcorios superiores").into(),
-        );
-    }
-
-    ruta_buf = PathBuf::from("../view");
-
-    ruta_buf.push(r);
-
-    if !ruta_buf.exists() {
-        return Err(tauri::Error::InvalidWindowUrl("Ruta incorrecta").into());
-    }
-
+async fn abrir_nueva_ventana(
+    handle: tauri::AppHandle,
+    ruta: &str,
+    titulo: &str,
+) -> Result<&'static str, CustomError> {
     match handle.get_window("secundaria") {
         None => {
-            let _docs_window = tauri::WindowBuilder::new(
-                &handle,
-                "secundaria", /* the unique window label */
-                tauri::WindowUrl::App(ruta_buf),
-            )
-            .menu(crear_barra_de_menu())
-            .title(titulo)
-            .center()
-            .build()?;
+            let mut ruta_buf: PathBuf;
+
+            let r = ruta.parse::<PathBuf>()?;
+
+            ruta_buf = PathBuf::from("../view");
+
+            ruta_buf.push(r);
+
+            if !ruta_buf.exists() {
+                return Err(tauri::Error::InvalidWindowUrl("Ruta incorrecta").into());
+            }
+
+            let _ =
+                tauri::WindowBuilder::new(&handle, "secundaria", tauri::WindowUrl::App(ruta_buf))
+                    .menu(crear_barra_de_menu())
+                    .title(titulo)
+                    .center()
+                    .build()?;
+
             Ok("Ventana creada")
         }
         Some(ventana) => {
+            let ruta = format!("/view/{ruta}");
+            if ventana.url().path() != ruta {
+                ventana.eval(
+                    "window.location.replace(new URL('/inditex.html', window.location.href))",
+                )?;
+            }
+
             ventana.set_focus()?;
             Ok("La ventana ya existe")
         }
@@ -71,10 +74,10 @@ fn menu_handler(evento: WindowMenuEvent) {
             let _ = evento
                 .window()
                 .app_handle()
-                .emit_all("cerrar_ventanas", "da")
+                .emit_all("cerrar_ventanas", ())
                 .unwrap_or_else(|e| {
-                    let error = TauriError(e);
-                    info!(error; "Error al emitir el evento");
+                    let e = TauriError(e);
+                    info!(e; "Error al emitir el evento");
                 });
         }
         _ => {}
@@ -82,6 +85,8 @@ fn menu_handler(evento: WindowMenuEvent) {
 }
 
 fn main() {
+    //En las build de desarrollo los logs se registran en Stdout y Webview(consola del navegador).
+    //En las builds finales, sera en los en el direcotorio especifo para logs
     #[cfg(debug_assertions)]
     let target: [LogTarget; 2] = [LogTarget::Stdout, LogTarget::Webview];
     #[cfg(not(debug_assertions))]
