08366f8a244df74091afe1fcc38d67b4b4d89da7
Luis Puente Fernández <luis.p.fdez9@gmail.com>
2024-11-17 21:37:07 +0100

    Modificadas APIs

--- src/interfaces/IPokeApi.ts
+++ src/interfaces/IPokeApi.ts
@@ -10,29 +10,30 @@
  * que interfaz hace refererencia. En caso de no estar definida, pasar unknown como generico
  */
 interface NamedAPIResource<T> {
-    readonly type: T,
-    name: string,
-    url: string
+    readonly type: T;
+    name: string;
+    url: string;
 }
 
 interface EvolutionChain {
-    id: number,
+    id: number;
     chain: Chain;
 }
 
 interface Chain {
-    is_baby: boolean,
-    species: NamedAPIResource<PokemonSpecies>
-    evolution_details: Array<unknown>,
-    evolves_to: Array<Chain>,
+    is_baby: boolean;
+    species: NamedAPIResource<PokemonSpecies>;
+    evolution_details: Array<unknown>;
+    evolves_to: Array<Chain>;
 }
 
 interface Pokemon {
-    id: number,
-    name: string,
-    sprites: PokemonSprites
-    cries: PokemonCries,
-    types: Array<PokemonType>
+    id: number;
+    name: string;
+    forms: NamedAPIResource<unknown>;
+    sprites: PokemonSprites;
+    cries: PokemonCries;
+    types: Array<PokemonType>;
 }
 
 interface PokemonCries {
@@ -51,32 +52,49 @@ interface PokemonSprites {
     front_shiny_female?: string;
 }
 
+interface PokemonFormSprites {
+    front_default: string;
+    front_shiny: string;
+    back_default: string;
+    back_shiny: string;
+}
+
 interface PokemonType {
     slot: number;
     type: NamedAPIResource<unknown>;
 }
 
 interface PokemonSpecies {
-    id: number,
-    name: string,
-    order: number,
-    gender_rate: number,
-    is_baby: boolean,
-    is_legendary: boolean,
-    is_mythical: boolean,
-    has_gender_differences: boolean,
-    forms_switchable: boolean,
-    varieties: Array<PokemonSpeciesVariety>,
-    flavor_text_entries: Array<FlavorText>
+    id: number;
+    name: string;
+    order: number;
+    gender_rate: number;
+    is_baby: boolean;
+    is_legendary: boolean;
+    is_mythical: boolean;
+    has_gender_differences: boolean;
+    forms_switchable: boolean;
+    varieties: Array<PokemonSpeciesVariety>;
+    flavor_text_entries: Array<FlavorText>;
 }
 
 interface PokemonSpeciesVariety {
-    is_default: boolean,
-    pokemon: NamedAPIResource<Pokemon>
+    is_default: boolean;
+    pokemon: NamedAPIResource<Pokemon>;
 }
 
 interface FlavorText {
-    flavor_text: string,
-    language: NamedAPIResource<unknown>,
-    version: NamedAPIResource<unknown>
+    flavor_text: string;
+    language: NamedAPIResource<unknown>;
+    version: NamedAPIResource<unknown>;
+}
+
+interface PokemonForm {
+    name: string;
+    form_name: string;
+    is_battle_only: boolean;
+    is_mega: boolean;
+    pokemon: NamedAPIResource<Pokemon>;
+    types: Array<PokemonType>;
+    sprites: PokemonFormSprites;
 }
